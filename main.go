fa03aa7f245d7242db3afcca036da5fbb759da67
Atila Romero <atilaromero@gmail.com>
2020-03-30 12:03:15 -0300

    add host argument

--- main.go
+++ main.go
@@ -9,18 +9,19 @@ import (
 )
 
 func main() {
-	usage := "Usage: allports [start] [end]"
-	if len(os.Args) != 3 {
+	usage := "Usage: allports [host] [start] [end]"
+	if len(os.Args) != 4 {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
-	start, err := strconv.Atoi(os.Args[1])
+	host := os.Args[1]
+	start, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		fmt.Println("[start] must be a number")
 		fmt.Println(usage)
 		os.Exit(1)
 	}
-	end, err := strconv.Atoi(os.Args[2])
+	end, err := strconv.Atoi(os.Args[3])
 	if err != nil {
 		fmt.Println("[end] must be a number")
 		fmt.Println(usage)
@@ -31,22 +32,22 @@ func main() {
 		ports = append(ports, i)
 	}
 
-	mkServers(ports)
+	mkServers(host, ports)
 }
 
-func mkServers(ports []int) {
+func mkServers(host string, ports []int) {
 	wg := sync.WaitGroup{}
 	for _, port := range ports {
 		wg.Add(1)
-		go listen(port, &wg)
+		go listen(host, port, &wg)
 	}
 	wg.Wait()
 }
 
-func listen(port int, wg *sync.WaitGroup) {
+func listen(host string, port int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	handler := mkHandler(port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), handler)
 	if err != nil {
 		fmt.Println(err)
 	}
